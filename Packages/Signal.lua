24ede8749530f289a3b16e10fe6611ad6744a5ed
Mark Langen <stravant@gmail.com>
2026-02-10 23:30:40 -0800

    Library update in preparation for port

--- Packages/Signal.lua
+++ Packages/Signal.lua
@@ -1,1 +1,4 @@
-return require(script.Parent._Index["stravant_goodsignal@0.2.1"]["goodsignal"])
+local REQUIRED_MODULE = require(script.Parent._Index["stravant_goodsignal@0.3.1"]["goodsignal"])
+export type Connection = REQUIRED_MODULE.Connection 
+export type Signal<T...> = REQUIRED_MODULE.Signal<T...>
+return REQUIRED_MODULE
--- Packages/createSharedToolbar.lua
+++ Packages/createSharedToolbar.lua
@@ -1,1 +1,3 @@
-return require(script.Parent._Index["stravant_create-shared-toolbar@0.1.0"]["create-shared-toolbar"])
+local REQUIRED_MODULE = require(script.Parent._Index["stravant_create-shared-toolbar@0.1.0"]["create-shared-toolbar"])
+export type SharedToolbarSettings = REQUIRED_MODULE.SharedToolbarSettings 
+return REQUIRED_MODULE
