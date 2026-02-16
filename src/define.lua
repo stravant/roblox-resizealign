a1a118213542efdeeac73dedf83e56b7dd5ea38c
Mark Langen <stravant@gmail.com>
2026-02-16 00:44:10 -0800

    Add new icon -> V2.0 release

--- src/define.lua
+++ src/define.lua
@@ -1,6 +1,6 @@
 --!strict
 return {
 	PluginName = "ResizeAlign",
-	ButtonIcon = "rbxassetid://4524348910",
+	ButtonIcon = "rbxassetid://77427491603137",
 	ButtonTooltip = "Resize pairs of parts up to the point where they intersect.",
 }
